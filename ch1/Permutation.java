bc3edaee42bc0249ee7c8bc988067739f794c838
Kazi Rahman -X (kazrahma - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kazrahma@cisco.com>
2018-03-11 13:13:23 -0700

    stuff

---
+++ ch1/Permutation.java
@@ -0,0 +1,21 @@
+
+class Permutation{
+
+	public static void main (String args[]){
+		//Scanner sc = new Scanner(System.in);
+		String sent = "abc";
+		String boom = "cba";
+		
+		
+		System.out.println(isP(sent,boom));
+		
+		
+	}
+	
+	
+	public static boolean isP(String s1, String s2){
+		
+		return false;
+	}
+
+}
--- ch1/uniqueString.java
+++ ch1/uniqueString.java
@@ -1,35 +1,35 @@
 import java.util.*;
-import java.io.*;
 
 
 class uniqueString{
-
-   public static void main(String[] args){
-      Scanner in = new Scanner(System.in);
-      String str = in.nextLine();
-         
-      if(check(str) == true){
-         System.out.println("unique");
-      
-      }else{ System.out.println("not unique");}
-
-
-
-   }
-
-   public static boolean check(String str){
-      boolean[] ch = new boolean[256];
-      char[] newArr = str.toCharArray();
-      for(int i = 0; i < str.length(); i++){
-         int val = newArr[i];
-         if (ch[val] == true){
-             return false;
-         }else{
-            ch[val] = true;
-         }
-
-      }
-      return true;
-
-   }
+	
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String ent = sc.nextLine();
+		boolean unique = isUnique(ent);
+		
+		System.out.println("The string is unique? " + unique);
+		
+	}
+	
+	public static boolean isUnique(String sent){
+		sent = sent.replaceAll("\\s+","");
+		int n = sent.length();
+		boolean [] check = new boolean[256];
+		for(int i = 0; i < n; i++){
+			int val = sent.charAt(i);
+			if(check[val] == true){
+				return false;
+			}else{check[val] = true;}
+			
+			
+		}
+		
+		//System.out.println(n);
+		return true;
+	}
+	
+	
+	
 }
