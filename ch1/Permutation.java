15f01068f8c3668f8d0b295a2ddc2f8c06cb2795
Kazi Rahman -X (kazrahma - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kazrahma@cisco.com>
2018-03-13 13:09:01 -0700

    updated working

--- ch1/Permutation.java
+++ ch1/Permutation.java
@@ -1,21 +1,83 @@
+import java.util.*;
+import java.io.*;
 
 class Permutation{
 
 	public static void main (String args[]){
-		//Scanner sc = new Scanner(System.in);
-		String sent = "abc";
-		String boom = "cba";
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> list = new ArrayList<String>();
+		String jav = "";
+		Random rand = new Random();
+	
+		try{
+			Scanner in = new Scanner(new File("./words.txt"));
+			while(in.hasNextLine()){
+				list.add(in.nextLine());
+			}
+		}catch(Exception e){
+			System.out.println(e);
+		}
 		
+		jav = list.get(rand.nextInt(list.size())+1);
 		
-		System.out.println(isP(sent,boom));
+		System.out.println("Give a permutation of the word "+jav);
 		
+		String boom = sc.nextLine();
+		
+		char b = 'b';
+		int ba = (int)b;
+		char c = (char)ba;
+		boolean ti = isP(jav,boom);
+		
+		while (!ti){
+			System.out.println("Not a permutation of the word "+jav);
+			System.out.println("Try again or quit by pressing 'n'");
+			String boom2 = sc.nextLine();
+				if(boom2.equals("n")){
+					System.exit(0);
+				}
+			ti = isP(jav,boom2);
+		}
+		
+		System.out.println("Nice!");
 		
 	}
 	
 	
 	public static boolean isP(String s1, String s2){
+		if(s1.length() != s2.length()) return false;
 		
-		return false;
+		int n = s1.length();
+		
+		char check1[] = s1.toCharArray();
+		char check2[] = s2.toCharArray();
+		
+		for(int i = 0; i < n - 1; i++){
+			int min_i = i;
+			for(int j = i; j < n; j ++){
+				if(check1[min_i] > check1[j]){
+					min_i = j;
+				}
+			}
+			char temp = check1[min_i];
+			check1[min_i] = check1[i];
+			check1[i] = temp;
+		}
+		
+		for(int i = 0; i < n - 1; i++){
+			int min_i = i;
+			for(int j = i; j < n; j ++){
+				if(check2[min_i] > check2[j]){
+					min_i = j;
+				}
+			}
+			char temp = check2[min_i];
+			check2[min_i] = check2[i];
+			check2[i] = temp;
+		}
+		
+		return Arrays.equals(check1, check2);
 	}
+	
 
 }
--- ch1/uniqueString.java
+++ ch1/uniqueString.java
@@ -7,9 +7,25 @@ class uniqueString{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string: ");
 		String ent = sc.nextLine();
+		
+		long startTime = System.nanoTime();
+		
 		boolean unique = isUnique(ent);
 		
-		System.out.println("The string is unique? " + unique);
+		long endTime = System.nanoTime();
+		long durationNoExtra = (endTime - startTime);
+		
+		System.out.println(durationNoExtra);
+		
+		
+		long startTime1 = System.nanoTime();
+		//longer??
+		boolean lol = uniqueNoDataStructure(ent);
+		long endTime1 = System.nanoTime();
+		long durationExtra = (endTime1 - startTime1);
+		System.out.println("no extra struct "+durationExtra);
+		
+		System.out.println("The string is unique? " + unique + " " + lol);
 		
 	}
 	
@@ -28,6 +44,19 @@ class uniqueString{
 		
 		//System.out.println(n);
 		return true;
+	}
+	
+	public static boolean uniqueNoDataStructure(String ent){
+		ent = ent.replaceAll("\\s","");
+		for(int i = 0; i < ent.length(); i++){
+			for(int j =  i+1; j < ent.length(); j++){
+				if(ent.charAt(i) == ent.charAt(j)){
+					return false;
+				}
+			}
+		}
+		return true;
+		
 	}
 	
 	
