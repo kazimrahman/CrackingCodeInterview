aeddfa0e865179234568541358e1ef25b2494577
Kazi Rahman -X (kazrahma - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kazrahma@cisco.com>
2018-03-22 23:22:17 -0700

    working on string compress

--- ch1/stringComp.java
+++ ch1/stringComp.java
@@ -1,15 +1,27 @@
 
 class stringComp{
 	
-	public static void main(Stirng[] args){
+	public static void main(String[] args){
 	
-		String b = "aaabbcccaaaa"
+		String b = "aaabbcccccd";
 		
 		String c = compress(b);
 	}
 	
 	static String compress(String a){
 		
+		int i = 0;
+		
+		while(i < a.length()-1){
+			int count = 1;
+			int j = i;
+			while(a.charAt(i) == a.charAt(j+1)){
+				count++;
+				j++;
+			}
+			i = j+1;
+			System.out.println(count);
+		}
 		
 		
 		
