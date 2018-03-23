3ed6e7af9545d1d971c851257cd7c8e7dccef9be
Kazi Rahman -X (kazrahma - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kazrahma@cisco.com>
2018-03-23 16:38:01 -0700

    working string program

--- ch1/stringComp.java
+++ ch1/stringComp.java
@@ -1,31 +1,41 @@
+import java.util.*;
 
 class stringComp{
-	
 	public static void main(String[] args){
 	
-		String b = "aaabbcccccd";
+		Scanner in = new Scanner(System.in);
 		
-		String c = compress(b);
+		System.out.println("Enter string to compress");
+		String b = in.nextLine();
+		
+		System.out.println(compress(b));
+		System.out.println(concat(b));
+		
 	}
 	
 	static String compress(String a){
 		
-		int i = 0;
+		int count = 0;
+		StringBuilder s = new StringBuilder("");
 		
-		while(i < a.length()-1){
-			int count = 1;
-			int j = i;
-			while(a.charAt(i) == a.charAt(j+1)){
-				count++;
-				j++;
+		for(int i = 0; i < a.length(); i++){
+			count++;
+			if(i+1 >= a.length() || a.charAt(i) != a.charAt(i+1)){
+				s.append(a.charAt(i));
+				s.append(count);
+				count = 0;
 			}
-			i = j+1;
-			System.out.println(count);
 		}
 		
+		return (a.length() >s.length()) ? (s.toString()) : (a);
 		
+		//return a;
+	}
+	
+	static String concat(String a){
+		if(a == null || a.length() <= 1){return a;}
+		return concat(a.substring(1,a.length())) + a.charAt(0); 
 		
-		return a;
 	}
 
 }
